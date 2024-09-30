e1f9d9267bc43ffe51c8abc6d00b17e040ac33c6
popvaxDev <tanul@popvax.com>
2024-10-01 00:31:47 +0530

    docker-react-automation

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:18-alpine
+
+WORKDIR /app
+
+COPY . .
+
+RUN npm install
+
+EXPOSE 8080
+
+CMD [ "npm", "run", "dev" ]
+
+
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>testing auto mation </h1>
+      <h2>hello</h2>
+    </div>
+  );
+}
+
+export default App;
